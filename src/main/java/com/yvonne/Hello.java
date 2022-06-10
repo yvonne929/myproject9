952a9d8e283aa19c50c9fd020660ff1b3c21e049
yu hsuan <yvonne20030929@gmail.com>
2022-06-10 15:16:32 +0800

    Hello world with class Person

---
+++ src/main/java/com/yvonne/Hello.java
@@ -0,0 +1,9 @@
+package com.yvonne;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello");
+        Person p = new Person();
+        p.greeting();
+    }
+}
