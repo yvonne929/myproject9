fe6d8b00fc6f6d62e267894dcdf0ecd963bb653f
yu hsuan <yvonne20030929@gmail.com>
2022-06-11 11:36:44 +0800

    PersonKotlin 補充

--- src/main/java/PersonKotlin.kt
+++ src/main/java/PersonKotlin.kt
@@ -1,12 +1,18 @@
-package com.yvonne
+package com.yvonne//可不加分號
 
-class PersonKotlin{
+class PersonKotlin{//class前不用加public,沒有寫就是public
     fun greeting(){
         println("Hello kotlin")
     }
 }
 
-fun main(){
+
+//top-level 第一級
+fun main(){//main方法可不在類別(class)內
     val p = PersonKotlin()
+    /*
+    val -> 不可改變的值
+    var -> 可改變的值
+     */
     p.greeting()
 }
