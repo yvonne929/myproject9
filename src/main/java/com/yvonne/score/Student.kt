e6b6778b9986ab3ab001f4cfd944b9df4410aaa9
yu hsuan <yvonne20030929@gmail.com>
2022-06-17 15:20:52 +0800

    Student with kotlin

--- src/main/java/com/yvonne/score/Student.kt
+++ src/main/java/com/yvonne/score/Student.kt
@@ -1,12 +1,26 @@
 package com.yvonne.com.yvonne.score
 
-class Student {
-    //屬性
+class Student(/*屬性*/ var name:String , var english:Int, var math:Int) {
+    /*屬性(java方式)
     var name = ""
     var english = 0
     var math = 0
+     */
+    init {
+    // println("Testing)
+    }
+
+    fun print(){
+        //原: println(name + "\t" + english + "\t" + math)
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+
+    fun getAverage():Int{
+        return (english+math)/2
+    }
 }
 
 fun main() {
     val student = Student("Jack",80,80)
+    student.print()
 }
