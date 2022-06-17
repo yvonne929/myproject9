6d00b56540fcb7ff2b13e6db48351ee3d3dcb45c
yu hsuan <yvonne20030929@gmail.com>
2022-06-17 14:42:21 +0800

    initial writing class Student with kotlin

---
+++ src/main/java/com/yvonne/score/Student.kt
@@ -0,0 +1,12 @@
+package com.yvonne.com.yvonne.score
+
+class Student {
+    //屬性
+    var name = ""
+    var english = 0
+    var math = 0
+}
+
+fun main() {
+    val student = Student("Jack",80,80)
+}
